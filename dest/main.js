d9f08ee65f2ac5866ab374532927cf4947902a2d
PhuongDam233 <phuongdamthu233@gmail.com>
2024-11-14 20:40:31 +0700

    Orangy-Tabs: set Js tabs transform category

---
+++ dest/main.js
@@ -0,0 +1,27 @@
+function handleTabsNew() {
+  let tabs = document.querySelectorAll(".sctabs__tab-item"),
+    listNews = document.querySelectorAll(".sctabs__wrap .article__list");
+
+  tabs.forEach(function (tab) {
+    tab.addEventListener("click", function () {
+      // remove all class active of tabs
+      tabs.forEach(function (tab) {
+        tab.classList.remove("active");
+      });
+
+      //   add class active
+      this.classList.add("active");
+
+      // hide all news list
+      listNews.forEach(function (newList) {
+        newList.classList.remove("active");
+      });
+
+      // get data-tab
+      let id = this.dataset.tab;
+      // addclass active to news list
+      document.querySelector(".article__list-" + id).classList.add("active");
+    });
+  });
+}
+handleTabsNew();
