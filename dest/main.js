e2f477a1adea99d3e3ff5911bbc9b4310afaeabf
PhuongDam233 <phuongdamthu233@gmail.com>
2024-12-01 22:45:32 +0700

    Orangy : add smooth scrolling on web

--- dest/main.js
+++ dest/main.js
@@ -435,3 +435,13 @@ function handleCookieBar() {
   });
 }
 handleCookieBar();
+
+// LENIS
+// Initialize Lenis
+const lenis = new Lenis({
+  autoRaf: true,
+});
+// Listen for the scroll event and log the event data
+lenis.on("scroll", (e) => {
+  console.log(e);
+});
